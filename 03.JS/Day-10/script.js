cb792856ddc7a75337dada69d16a8f0dd07031b7
InnovateTechnologiesChennai <innovatetechnologieschn@gmail.com>
2024-06-13 12:02:32 +0530

    Day 10 Updated

---
+++ 03.JS/Day-10/script.js
@@ -0,0 +1,103 @@
+// console.log('Hello world.. 1234 !#$&(&')
+// console.log(1234);
+// console.log(true);
+
+// datatypes : 
+// primitive & non primitive
+
+// primitive - 
+
+// string = mixed of alphabets , numbers , spe characters =  ("" '')
+// number =  
+// boolean = true or false
+// undefined 
+// null
+
+// non primitive = object function array
+
+// variables = 3 => var let  const 
+
+// let  myname = "MS DHONI"
+// console.log(myname);
+// console.log(typeof(myname));
+
+// let  myage = 42
+// console.log(myage);
+// console.log(typeof(myage));
+
+// let isPlayer = false
+// console.log(isPlayer);
+// console.log(typeof(isPlayer));
+
+// let teamName 
+// console.log(teamName);
+// console.log(typeof(teamName));
+
+// let place = null // s n b  non-primitive(o)
+// console.log(place);
+// console.log(typeof(place)); // object //  str num boolean obj func array
+
+// string  +   (concat) 
+// number + (addition)
+
+let fName = "MS"
+let lName = "Dhoni" 
+let age = 42
+
+// console.log(fName + ' ' + lName);
+// My name is MS Dhoni, My age is 42
+
+console.log("My name is " + fName + " " + lName+", My age is " + age);
+
+// string / Template literals
+
+console.log(`My name is ${fName} ${lName}, My age is ${age}`);
+
+// operators 
+// arithmetic operator (+-*/)
+
+let num1= 10
+let num2 = 20
+console.log(num1/num2);
+
+// assignment 
+
+let x = 5
+//  x = x+5 // x+=5
+// x = x-5 // x-=5
+// x= x*5//x*=5
+// x=x/5 // x/=5
+ console.log(x);
+
+ // equality signs
+ // = (assign)
+ // == (equal to)
+ // === (compare)
+ // !== (not equal to)
+
+
+ // comparison - boolean - (true , false)
+
+ let z = 5
+ console.log( z < 5 ); // true or false (5>5)
+
+ // logical operator (and , or)
+  let a = 10
+  let b = 15
+
+  console.log((a>20) && (b>5)); // (10>20) || (15>5)  // f || t = true
+
+// increment  (++pre , post++) / decrement  (pre , post)
+
+let c = 5
+++c
+c++
+++c
+c--
+++c
+--c
+c--
+++c
+console.log(c);
+
+
